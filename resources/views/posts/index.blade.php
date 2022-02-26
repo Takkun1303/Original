74c99386a962d7f4c5424bb9ba2f33765de2dae6
Takkun1303 <yamamoto.takumi.009@s.kyushu-u.ac.jp>
2022-02-26 15:24:08 +0900

    8-5

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,8 +12,7 @@
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
-                    <h2 class='title'><a href="/posts/{{$post->id}}">{{$post->title}}</a></h2>
-
+                    
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
@@ -24,7 +23,7 @@
 
         [<a href="/posts/create">create</a>]
 
-        <a href='/posts/create'>create</a>
+        
 
     </body>
 </html>
